ddcbb05c6b32cda6f918452daa92b420970fe4ec
Farouk Allani <81933547+farouk-allani@users.noreply.github.com>
2023-01-03 17:53:19 +0100

    feat: create theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,8 @@
+import { BrowserRouter,Navigate, Routes , Route } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+import Navbar from 'scenes/navbar';
 import './App.css';
 
 function App() {
